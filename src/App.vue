5d977a03b4f48f81cc42beba7200eac670f22c90
IRSAGE <sibo.egide98@gmail.com>
2022-10-26 19:08:12 +0200

    Designing the Task component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div class="container"><Header title="Task Tracker"/></div>
+  <div class="container">
+    <Header title="Task Tracker" />
+    <Tasks :tasks="tasks" />
+  </div>
 </template>
 
 <script>
 
 import Header from "./components/Header"
+import Tasks from "./components/Tasks"
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: 'March 1st at 2:30pm',
+        reminder: true
+      },
+      {
+        id: 2,
+        text: "Shoping",
+        day: 'March 1st at 2:30pm',
+        reminder: true
+      }
+    ]
   }
 }
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
+
 body {
   font-family: 'Poppins', sans-serif;
 }
+
 .container {
   max-width: 500px;
   margin: 30px auto;
@@ -32,6 +61,7 @@ body {
   padding: 30px;
   border-radius: 5px;
 }
+
 .btn {
   display: inline-block;
   background: #000;
@@ -45,12 +75,15 @@ body {
   font-size: 15px;
   font-family: inherit;
 }
+
 .btn:focus {
   outline: none;
 }
+
 .btn:active {
   transform: scale(0.98);
 }
+
 .btn-block {
   display: block;
   width: 100%;
